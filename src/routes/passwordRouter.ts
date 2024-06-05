4cba3f848cb15e4e7c08346d4f212053feceb0c8
yasser mst <mestaoui0yasser@gmail.com>
2024-06-05 19:19:27 +0100

    changa password

---
+++ src/routes/passwordRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { updatePassword  } from "../controllers/passwordController";
+
+const routerPassword = Router();
+
+
+routerPassword.post("/password", updatePassword);
+
+
+export { routerPassword };
