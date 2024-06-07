a3adcd7d3319ab27732d4b2de267d0e5aa1cc782
yasser mst <mestaoui0yasser@gmail.com>
2024-06-07 12:58:58 +0100

    validation mail+artist w AYI la3ba

--- src/routes/artistRouter.ts
+++ src/routes/artistRouter.ts
@@ -8,14 +8,15 @@ import {
 import { Router } from "express";
 import upload from "../middlewares/uploadImage";
 import { verifyToken } from "../middlewares/verifyToken";
+import { validateArtist } from "../middlewares/validationArtiste";
 
 const routerArtist = Router();
 
 routerArtist.use(verifyToken);
 routerArtist.get("/", getAllArtists);
 routerArtist.get("/:firstName/:lastName?", getArtistByName);
-routerArtist.post("/addArtist", upload.single("image"), addArtist);
+routerArtist.post("/addArtist", upload.single("image"), validateArtist, addArtist);
 routerArtist.delete("/deleteArtist/:_id", deleteArtist);
-routerArtist.put("/updateArtist/:_id", upload.single("image"), updateArtist);
+routerArtist.put("/updateArtist/:_id", upload.single("image"), validateArtist, updateArtist);
 
 export { routerArtist };
--- src/routes/newsletterRouter.ts
+++ src/routes/newsletterRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { newsletter, unsubscribe } from "../controllers/mailController";
+import { validateEmail } from "../middlewares/validationMail"
 
 const routerMail = Router();
 
-routerMail.post("/", newsletter);
+routerMail.post("/",validateEmail, newsletter);
 routerMail.post("/unsubscribe", unsubscribe);
 
 export { routerMail };
